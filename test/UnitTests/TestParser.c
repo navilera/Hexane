c232b242f55224f121baba5e2f49edb7c9b1851e
Manwoo Yi <navilera@gmail.com>
2017-08-27 02:39:02 -0700

    Add test cases

--- test/UnitTests/TestParser.c
+++ test/UnitTests/TestParser.c
@@ -29,6 +29,12 @@ static bool commonTestType(char* line, Bnf_t testType, int* targetDepth)
 	Symbol_t* symlist = Lexer_GetSym(line);
 	ParserNode_t* parseTree = Parser_Parse(symlist);
 
+	if(parseTree == NULL)
+	{
+		printf(TESTCASEMSGINDENT "FAIL.. Syntax Error\n");
+		return false;
+	}
+
 	ParserNode_t* parseTreeIndex = parseTree;
 	for(int* idx = targetDepth ; *idx ; ++idx)
 	{
@@ -100,3 +106,39 @@ TESTCASE(simpleExprTest_addVal, "Simple Expression Test Add Value")
 	int depth[3] = {1, 2, 0};
 	return commonTestVal(line, 0x15, depth);
 }
+
+TESTCASE(complexExprTest_addsub, "Complex Expression Test AddSub")
+{
+	char* line = "c0ffee + 15 - 10 + 20\n";
+	int depth[30] = {1, 1, 2, 0};
+	return commonTestVal(line, 0x10, depth);
+}
+
+TESTCASE(complexExprTest_addmul, "Complex Expression Test AddMul")
+{
+	char* line = "c0ffee + 15 - 10 + 20 * 30\n";
+	int depth[30] = {1, 2, 2, 0};
+	return commonTestVal(line, 0x30, depth);
+}
+
+TESTCASE(complexExprTest_addmulmix01, "Complex Expression Test AddMulMix01")
+{
+	char* line = "c0ffee + 15 - 10 + 20 * 30+AB*30+60+CD*EFF*AD\n";
+	int depth[30] = {1, 1, 1, 2, 1, 0};
+	return commonTestVal(line, 0xAB, depth);
+}
+
+TESTCASE(complexExprTest_addmulmix02, "Complex Expression Test AddMulMix02")
+{
+	char* line = "c0ffee + 15 - 10 + 20 * 30+AB*30+60+CD*EFF*AD\n";
+	int depth[30] = {1, 1, 1, 2, 1, 0};
+	return commonTestVal(line, 0xAB, depth);
+}
+
+TESTCASE(complexExprTest_addmulmix03, "Complex Expression Test AddMulMix03")
+{
+	char* line = "c0ffee + 15 - 10 + 20 * 30+AB*30+60+CD*EFF*AD\n";
+	int depth[30] = {1, 2, 1, 0};
+	return commonTestType(line, BNF_mul, depth);
+}
+
